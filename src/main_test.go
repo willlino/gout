4e0848f7c4195bd88ab3ddc284ec5882fc08a683
= <=>
2020-04-08 10:41:49 -0300

    teste

--- src/main_test.go
+++ src/main_test.go
@@ -3,14 +3,14 @@ package main
 import "testing"
 
 
-func TestSoma( t *testing.T ){
-	result := soma(5, 5)
+func TestSomar( t *testing.T ){
+	result := somar(5, 5)
 
 	if result != 10 {
 		t.Errorf("soma falhou, valor 10 esperado, mas o valor está %v", result)
 	}
 
-	result2 := soma(2, 3)
+	result2 := somar(2, 3)
 
 	if(result2) != 5 {
 		t.Errorf("soma falhou, valor 5 esperado, mas o valor está %v", result2)
